d421974d84b468cb9a53966e4a5b1e4615a42df2
Braelyn Boynton <bboynton97@gmail.com>
2024-09-26 13:52:42 -0700

    some crew template stuff

---
+++ templates/crewai/{{cookiecutter.project_slug}}/src/crew.py
@@ -0,0 +1,38 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+
+@CrewBase
+class {{cookiecutter.project_name}}Crew():
+	"""{{cookiecutter.project_name}} crew"""
+
+	# Agent definitions
+    { % - for agent in cookiecutter.crewai.agents %}
+    @agent
+    def {{agent.name}}(self) -> Agent:
+        return Agent(
+            config=self.agents_config['{{agent.name}}'],
+            # tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
+            verbose=True
+        )
+    { % - endfor %}
+
+	# Task definitions
+	{ % - for tasks in cookiecutter.crewai.tasks %}
+	@task
+	def {{task.name}}(self) -> Task:
+		return Task(
+			config=self.tasks_config['{{task.name}}'],
+		)
+	{ % - endfor %}
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Test crew"""
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
