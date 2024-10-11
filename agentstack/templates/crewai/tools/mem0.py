919e0a4cfb3752b43b0881a5f8c38cfaf12a24de
Braelyn Boynton <bboynton97@gmail.com>
2024-10-10 18:05:47 -0700

    remove poetry dependencies

---
+++ agentstack/templates/crewai/tools/mem0.py
@@ -0,0 +1,16 @@
+from mem0 import Memory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = {
+    "vector_store": {
+        "provider": "chroma",
+        "config": {
+            "collection_name": "chatbot_memory",
+            "path": "./chroma_db",
+        },
+    },
+}
+
+memory = Memory.from_config(config)
