80ec8aed039e2a8478a45c1e6a9689588b59c70d
Braelyn Boynton <bboynton97@gmail.com>
2024-11-08 15:37:06 -0800

    mem0 upgrade

--- agentstack/templates/crewai/tools/mem0_tool.py
+++ agentstack/templates/crewai/tools/mem0_tool.py
@@ -1,42 +1,42 @@
+import json
+
 from crewai_tools import tool
-from mem0 import Memory
+from mem0 import MemoryClient
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-# Optionally configure mem0 to use any other store
-# https://docs.mem0.ai/components/vectordbs/config#config
-config = {
-    "graph_store": {
-        "provider": "neo4j",
-        "config": {
-            "url": os.getenv("NEO4J_URL"),
-            "username": os.getenv("NEO4J_USERNAME", 'neo4j'),
-            "password": os.getenv("NEO4J_PASSWORD"),
-        }
-    },
-    "version": "v1.1"
-}
+# These functions can be extended by changing the user_id parameter
+# Memories are sorted by user_id
 
-memory = Memory.from_config(config)
+MEM0_API_KEY = os.getenv('MEM0_API_KEY')
+client = MemoryClient(api_key=MEM0_API_KEY)
 
-# These functions can be extended by taking an optional user_id parameter
-# https://docs.mem0.ai/integrations/multion#add-memories-to-mem0
-
+# These tools will only save information about the user
+# "Potato is a vegetable" is not a memory
+# "My favorite food is potatoes" IS a memory
 
 @tool("Write to Memory")
-def write_to_memory(data: str) -> str:
-    """Writes data to the memory store"""
-    result = memory.add(data)
-    return f"Memory added with ID: {result['id']}"
+def write_to_memory(user_message: str) -> str:
+    """
+    Writes data to the memory store for a user. The tool will decide what
+    specific information is important to store as memory.
+    """
+    messages = [
+        {"role": "user", "content": user_message},
+    ]
+    result = client.add(messages, user_id='default')  # configure user
+    return json.dumps(result)
 
 
 @tool("Read from Memory")
 def read_from_memory(query: str) -> str:
-    """Reads memories based on a query."""
-    memories = memory.search(query=query)
-    if memories["memories"]:
-        return "\n".join([mem["data"] for mem in memories["memories"]])
+    """
+    Reads memories related to user based on a query.
+    """
+    memories = client.search(query=query, user_id='default')
+    if memories:
+        return "\n".join([mem['memory'] for mem in memories])
     else:
         return "No relevant memories found."
