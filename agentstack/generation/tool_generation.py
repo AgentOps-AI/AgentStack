151e4164223b7fc9b29892a3c85df079b9d4cd18
Travis Dent <tcdent@gmail.com>
2024-11-15 08:41:39 -0800

    Restore agent tool definition, implement removing agent tool definition

--- agentstack/generation/tool_generation.py
+++ agentstack/generation/tool_generation.py
@@ -25,7 +25,7 @@ def add_tool(tool_name: str, path: Optional[str] = None):
         agentstack_json = open_json_file(f'{path}{AGENTSTACK_JSON_FILENAME}')
         
         if tool_name in agentstack_json.get('tools', []):
-            print(f"\033[31mTool '{tool_name}' is already installed\033[0m")
+            print(term_color(f'Tool {tool_name} is already installed', 'red'))
             sys.exit(1)
 
         with importlib.resources.path(f'agentstack.tools', f"{tool_name}.json") as tool_data_path:
@@ -35,6 +35,7 @@ def add_tool(tool_name: str, path: Optional[str] = None):
                 os.system(tool_data['package'])  # Install package
                 shutil.copy(tool_file_path, f'{path}src/tools/{tool_name}_tool.py')  # Move tool from package to project
                 add_tool_to_tools_init(tool_data, path)  # Export tool from tools dir
+                add_tool_to_agent_definition(framework, tool_data, path)  # Add tool to agent definition
                 # TODO If the tool is reinstalled .env variables will be duplicated.
                 insert_code_after_tag(f'{path}.env', '# Tools', [tool_data['env']], next_line=True)  # Add env var
                 insert_code_after_tag(f'{path}.env.example', '# Tools', [tool_data['env']], next_line=True)  # Add env var
@@ -63,13 +64,14 @@ def remove_tool(tool_name: str, path: Optional[str] = None):
         agentstack_json = open_json_file(f'{path}{AGENTSTACK_JSON_FILENAME}')
         
         if not tool_name in agentstack_json.get('tools', []):
-            print(f"\033[31mTool '{tool_name}' is not installed\033[0m")
+            print(term_color(f'Tool {tool_name} is not installed', 'red'))
             sys.exit(1)
 
         with importlib.resources.path(f'agentstack.tools', f"{tool_name}.json") as tool_data_path:
             tool_data = open_json_file(tool_data_path)
             os.remove(f'{path}src/tools/{tool_name}_tool.py')
             remove_tool_from_tools_init(tool_data, path)
+            remove_tool_from_agent_definition(framework, tool_data, path)
             # We don't remove the .env variables to preserve user data.
             
             agentstack_json['tools'].remove(tool_name)
@@ -78,7 +80,7 @@ def remove_tool(tool_name: str, path: Optional[str] = None):
             
             print(term_color(f'🔨 Tool {tool_name} removed from agentstack project successfully', 'green'))
             # TODO If we add an uninstall command to the tool_config json we can run it instead. 
-            print(term_color(f' To uninstall dependencies, reverse the install command: {tool_data["package"]}', 'blue'))
+            print(term_color(f'To uninstall dependencies, reverse the install command: {tool_data["package"]}', 'blue'))
 
 
 def _format_tool_import_statement(tool_data: dict):
@@ -102,12 +104,34 @@ def remove_tool_from_tools_init(tool_data: dict, path: str = ''):
                 print(line, end='')
 
 
+def _framework_filename(framework: str, path: str = ''):
+    if framework == 'crewai':
+        return f'{path}src/crew.py'
+
+    print(term_color(f'Unknown framework: {framework}', 'red'))
+    sys.exit(1)
+
+
+def add_tool_to_agent_definition(framework: str, tool_data: dict, path: str = ''):
+    filename = _framework_filename(framework, path)
+    with fileinput.input(files=filename, inplace=True) as f:
+        for line in f:
+            print(line.replace('tools=[', f'tools=[{"*" if tool_data.get("tools_bundled") else ""}tools.{", tools.".join([tool_name for tool_name in tool_data["tools"]])}, '), end='')
+
+
+def remove_tool_from_agent_definition(framework: str, tool_data: dict, path: str = ''):
+    filename = _framework_filename(framework, path)
+    with fileinput.input(files=filename, inplace=True) as f:
+        for line in f:
+            print(line.replace(f'{", ".join([f"tools.{tool_name}" for tool_name in tool_data["tools"]])}, ', ''), end='')
+
+
 def assert_tool_exists(tool_name: str, tools: dict):
     for cat in tools.keys():
         for tool_dict in tools[cat]:
             if tool_dict['name'] == tool_name:
                 return
 
-    print(f"\033[31mNo known AgentStack tool: '{tool_name}'\033[0m")
+    print(term_color(f'No known agentstack tool: {tool_name}', 'red'))
     sys.exit(1)
 
