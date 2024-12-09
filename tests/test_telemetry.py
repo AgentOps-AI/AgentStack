818daa40fbf70917892b4b855c128c9797ea1a6f
Travis Dent <tcdent@gmail.com>
2024-12-09 10:41:20 -0800

    Better test classname

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -3,7 +3,7 @@ import unittest
 from agentstack.utils import get_telemetry_opt_out
 
 
-class TestEnv(unittest.TestCase):
+class TelemetryTest(unittest.TestCase):
     def test_telemetry_opt_out_env_var_set(self):
         AGENTSTACK_TELEMETRY_OPT_OUT = os.getenv("AGENTSTACK_TELEMETRY_OPT_OUT")
         assert AGENTSTACK_TELEMETRY_OPT_OUT
