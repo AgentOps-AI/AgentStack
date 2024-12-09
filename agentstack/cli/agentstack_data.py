9e781bc9cf44278752f90674c0660be502d7b642
Travis Dent <tcdent@gmail.com>
2024-12-09 15:50:54 -0800

    Unnecessary import

--- agentstack/cli/agentstack_data.py
+++ agentstack/cli/agentstack_data.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime
-from typing import Optional, Union
+from typing import Optional
 
 from agentstack.utils import clean_input, get_version
 from agentstack.logger import log
