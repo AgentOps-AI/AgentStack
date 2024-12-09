3ddddabac2fcc781f772dd3a9bcd0e9f94944f6d
Travis Dent <tcdent@gmail.com>
2024-12-09 10:33:53 -0800

    Support disabling telemetry with an environment variable (`AGENTSTACK_TELEMETRY_OPT_OUT`). Disable telemetry in test suite. Add a test for telemetry. #105

---
+++ tests/test_telemetry.py
@@ -0,0 +1,12 @@
+import os
+import unittest
+from agentstack.utils import get_telemetry_opt_out
+
+class TestEnv(unittest.TestCase):
+    def test_telemetry_opt_out_env_var_set(self):
+        AGENTSTACK_TELEMETRY_OPT_OUT = os.getenv("AGENTSTACK_TELEMETRY_OPT_OUT")
+        assert AGENTSTACK_TELEMETRY_OPT_OUT
+
+    def test_telemetry_opt_out_set_in_test_environment(self):
+        assert get_telemetry_opt_out()
+
