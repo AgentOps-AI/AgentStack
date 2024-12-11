4357f273d2e8a4ea407c6d12c1f4b7452144c88f
jayfish0 <jason@tinyfish.io>
2024-12-10 23:03:27 -0800

    add agentql tool

--- agentstack/templates/crewai/tools/agentql_tool.py
+++ agentstack/templates/crewai/tools/agentql_tool.py
@@ -1,33 +1,53 @@
 import os
-import requests
 
-from crewai_tools import tool
+from pydantic import BaseModel, Field
+from typing import Optional, Type
+
+from crewai_tools import BaseTool
 
 from dotenv import load_dotenv
 load_dotenv()
 
-url = "https://api.agentql.com/v1/query-data"
+QUERY_DATA_ENDPOINT = "http://0.0.0.0:8080/v1/query-data"
 api_key = os.getenv("AGENTQL_API_KEY")
 
-@tool
-def query_agentql(url: str, query: str):
-    """
-    Query AgentQL with a query and return the result.
-    """
+class AgentQLQueryDataSchema(BaseModel):
+    url: str = Field(description="Website URL")
+    query: Optional[str] = Field(
+        default=None,
+        description="AgentQL query to scrape the url"
+    )
+    description: Optional[str] = Field(
+        default=None,
+        description="Natural language description of the data you want to scrape"
+    )
 
-    payload = {
-        "url": url,
-        "query": query
-    }
+class AgentQLQueryDataTool(BaseTool):
+    name: str = "query_data"
+    description: str = "Scrape a url with a given AgentQL query or a natural language description of the data you want to scrape."
+    args_schema: Type[BaseModel] = AgentQLQueryDataSchema
 
-    headers = {
-        "X-API-Key": f"{api_key}",
-        "Content-Type": "application/json"
-    }
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    response = requests.post(url, headers=headers, json=payload)
-    if response.status_code == 200 and response.text:
-        return response.text
-    else:
-        print(f"{response.status_code} - {response.text}")
-        return "Failed to query AgentQL"
+    def _run(
+        self,
+        url: str,
+        query: Optional[str] = None,
+        description: Optional[str] = None
+    ) -> dict:
+        if query: 
+            return {"data": [{"name": "T-Shirt", "price": 100}, {"name": "Pants", "price": 200}]}
+        elif description:
+            return {"data": [{"name": "Coat", "price": 300}, {"name": "Shoes", "price": 400}]}
+        else:
+            return {"error": "No query or description provided"}
+        
+def createTool(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper()
+
+@createTool
+def query_data():
+    return AgentQLQueryDataTool()
