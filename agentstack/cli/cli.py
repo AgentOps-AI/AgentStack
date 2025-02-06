6a3028027962f6cd68abe838a3363051af8a2408
Braelyn Boynton <bboynton97@gmail.com>
2025-02-05 23:44:24 -0800

    serve work

--- agentstack/cli/cli.py
+++ agentstack/cli/cli.py
@@ -391,6 +391,6 @@ def serve_project():
     # TODO: only silence output conditionally - maybe a debug or verbose option
     os.system("docker stop agentstack-local > /dev/null 2>&1")
     os.system("docker rm agentstack-local > /dev/null 2>&1")
-    with importlib.resources.path('agentstack.deploy', 'Dockerfile') as path:
+    with importlib.resources.path('agentstack.serve', 'Dockerfile') as path:
         os.system(f"docker build -t agent-service -f {path} .")
     os.system("docker run --name agentstack-local -p 6969:6969 agent-service")
