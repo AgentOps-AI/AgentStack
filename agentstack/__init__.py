8a6c464c92ef058b60987d8ecdcca6c2d2ab8398
Travis Dent <tcdent@gmail.com>
2025-02-14 13:34:52 -0800

    Cleanup imports.

--- agentstack/__init__.py
+++ agentstack/__init__.py
@@ -5,7 +5,7 @@ Methods that have been imported into this file are expected to be used by the
 end user inside their project.
 """
 
-from typing import Callable, Type, TypeAlias, ClassVar
+from typing import Callable, TypeAlias
 from pathlib import Path
 from agentstack import conf
 from agentstack.utils import get_framework
@@ -17,29 +17,32 @@ from agentstack._tools import get_tool
 from agentstack import frameworks
 
 ___all___ = [
-    "conf", 
-    "agent", 
-    "task", 
-    "tools", 
-    "get_tags", 
-    "get_framework", 
-    "get_tool", 
-    "get_agent", 
+    "conf",
+    "agent",
+    "task",
+    "tools",
+    "get_tags",
+    "get_framework",
+    "get_tool",
+    "get_agent",
     "get_all_agents",
     "get_all_agent_names",
-    "get_task", 
+    "get_task",
     "get_all_tasks",
     "get_all_task_names",
-    "get_inputs", 
+    "get_inputs",
 ]
+
 
 def agent(func):
     """
-    The `agent` decorator is used to mark a method that implements an Agent. 
+    The `agent` decorator is used to mark a method that implements an Agent.
     """
+
     def wrap(*args, **kwargs):
         """Does not alter the function's behavior; this is just a marker."""
         return func(*args, **kwargs)
+
     return wrap
 
 
@@ -47,9 +50,11 @@ def task(func):
     """
     The `task` decorator is used to mark a method that implements a Task.
     """
+
     def wrap(*args, **kwargs):
         """Does not alter the function's behavior; this is just a marker."""
         return func(*args, **kwargs)
+
     return wrap
 
 
@@ -63,10 +68,11 @@ def get_tags() -> list[str]:
 class ToolsMetaclass(type):
     """
     Metaclass for the public tools interface.
-    
+
     Define methods here to expose in the public API. Using a metaclass let's us
     use methods traditionally only available to instances on the class itself.
     """
+
     def __getitem__(cls, tool_name: str) -> list[Callable]:
         """
         Get a tool's callables by name with `agentstack.tools[tool_name]`
@@ -84,13 +90,13 @@ class ToolsMetaclass(type):
 class tools(metaclass=ToolsMetaclass):
     """
     Provides the public interface for accessing `agentstack._tools` methods and
-    types that we explicitly expose. 
-    
+    types that we explicitly expose.
+
     Access wrapped tools with `agentstack.tools[tool_name]`
-    
+
     Access tool permissions with `agentstack.tools.get_permissions(func)`
-    
+
     Access the tool Action type with `agentstack.tools.Action`
     """
-    Action: TypeAlias = _tools.Action
 
+    Action: TypeAlias = _tools.Action
